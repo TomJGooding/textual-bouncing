30b4e8369e8bf7c0228060f832e626ded697c60e
TomJGooding <101601846+TomJGooding@users.noreply.github.com>
2024-08-07 22:08:53 +0100

    feat: add color changes

--- bouncing.py
+++ bouncing.py
@@ -1,5 +1,18 @@
+from itertools import cycle
+
 from textual.app import App, ComposeResult
 from textual.widgets import Static
+
+WIDGET_COLORS = [
+    "cyan",
+    "orange",
+    "blue",
+    "yellow",
+    "red",
+    "violet",
+    "green",
+    "fuchsia",
+]
 
 
 class BouncingApp(App):
@@ -7,18 +20,20 @@ class BouncingApp(App):
     Static {
         width: 20;
         height: 10;
-        background: red;
     }
     """
 
     dx = 2
     dy = 1
 
+    colors_cycle = cycle(WIDGET_COLORS)
+
     def compose(self) -> ComposeResult:
         yield Static()
 
     def on_mount(self) -> None:
         widget = self.query_one(Static)
+        widget.styles.background = next(self.colors_cycle)
         widget.offset += (self.dx, self.dy)
 
         self.set_interval(1 / 10, self.update)
@@ -30,12 +45,14 @@ class BouncingApp(App):
             or widget.offset.x + widget.size.width >= self.size.width
         ):
             self.dx *= -1
+            widget.styles.background = next(self.colors_cycle)
 
         if (
             widget.offset.y <= 0
             or widget.offset.y + widget.size.height >= self.size.height
         ):
             self.dy *= -1
+            widget.styles.background = next(self.colors_cycle)
 
         widget.offset += (self.dx, self.dy)
 
